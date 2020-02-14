6a5aa64e6f653740df7d022ad723e7b411dab9b2
Exodary <krasimir_zlatkov@mail.bg>
2020-02-14 21:20:20 +0200

    Adding Solved Tasks for Telerik Academy HW(Arrays)

---
+++ C# Fundamentals - 2/1.Arrays/CompareArrays/CompareArrays/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompareArrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int size = int.Parse(Console.ReadLine());
+            int[] firstArray = new int[size];
+            int[] secondArray = new int[size];
+            bool equal = true;
+
+            for (int i = 0; i < firstArray.Length; i++)
+            {
+                firstArray[i] = int.Parse(Console.ReadLine());
+            }
+
+            for(int j = 0; j < secondArray.Length; j++)
+            {
+                secondArray[j] = int.Parse(Console.ReadLine());
+            }
+
+            for(int k = 0; k < firstArray.Length; k++)
+            {
+                if(firstArray[k] == secondArray[k])
+                {
+                    equal = true;
+                }
+                else
+                {
+                    equal = false;
+                }
+            }
+
+            if (equal)
+            {
+                Console.WriteLine("Equal");
+            }
+            else
+            {
+                Console.WriteLine("Not Equal");
+            }
+        }
+    }
+}
---
+++ C# Fundamentals - 2/1.Arrays/MaximalSequance/MaximalSequance/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaximalSequance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int[] numbers = new int[n];
+
+            int currentSequence = 0;
+            int maxSequence = 0;
+
+            for(int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = int.Parse(Console.ReadLine());
+            }
+
+            for(int i = 0; i < numbers.Length - 1; i++)
+            {
+                if(numbers[i] == numbers[i + 1])
+                {
+                    currentSequence++;
+                }
+                else
+                {
+                    if(currentSequence > maxSequence)
+                    {
+                        maxSequence = currentSequence;
+                    }
+                    currentSequence = 1;
+                }
+            }
+
+            if(currentSequence > maxSequence)
+            {
+                maxSequence = currentSequence;
+            }
+            Console.WriteLine(maxSequence);
+        }
+
+    }
+}
