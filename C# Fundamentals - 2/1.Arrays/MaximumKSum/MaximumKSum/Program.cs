364753f7c3b8158730d68a3787089cea4166777f
Exodary <krasimir_zlatkov@mail.bg>
2020-02-15 18:46:07 +0200

    Adding more solved tasks for Arrays

---
+++ C# Fundamentals - 2/1.Arrays/MaximumKSum/MaximumKSum/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaximumKSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int k = int.Parse(Console.ReadLine());
+
+            int[] numbers = new int[n];
+
+            int sum = 0;
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = int.Parse(Console.ReadLine());
+            }
+
+            Array.Sort(numbers);
+
+            Array.Reverse(numbers);
+            
+            for(int i = 0; i < k; i++)
+            {
+                sum += numbers[i];
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
---
+++ C# Fundamentals - 2/1.Arrays/SelectionSort/SelectionSort/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SelectionSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int[] numbers = new int[n];
+
+            int minNumber = 0;
+
+
+            for(int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = int.Parse(Console.ReadLine());
+            }
+
+            for(int i = 0; i < numbers.Length - 1; i++)
+            {
+                minNumber = i;
+
+                int j;
+
+                for(j = i + 1; j < numbers.Length; j++)
+                {
+                    if(numbers[j] < numbers[minNumber])
+                    {
+                        minNumber = j;
+                    }
+                }
+
+                if (minNumber != j)
+                {
+                    int swap = numbers[i];
+                    numbers[i] = numbers[minNumber];
+                    numbers[minNumber] = swap;
+                }
+            }
+
+            foreach(int number in numbers)
+            {
+                Console.WriteLine(number);
+            }
+    
+            
+        }
+    }
+}
