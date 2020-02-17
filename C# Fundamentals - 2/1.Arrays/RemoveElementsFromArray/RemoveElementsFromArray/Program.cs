2904c60b557a772c345ea337ed5f93e832394f9b
Exodary <krasimir_zlatkov@mail.bg>
2020-02-17 17:20:55 +0200

    Adding More Solved Tasks For Arrays

---
+++ C# Fundamentals - 2/1.Arrays/RemoveElementsFromArray/RemoveElementsFromArray/Program.cs
@@ -0,0 +1,72 @@
+﻿/*  Problem 18. Remove elements from array
+    Write a program that reads an array of integers and removes from it a minimal number of elements 
+    in such a way that the remaining array is sorted in increasing order. 
+    Print the minimal number of elements that need to be removed in order for the array to become sorted.
+    Input:
+            On the first line you will receive the number N
+            On the next N lines the numbers of the array will be given
+    Output: Print the minimal number of elements that need to be removed
+    Constraints: 1 <= N <= 1024                                             */
+
+// Algorithm => http://www.algorithmist.com/index.php/Longest_Increasing_Subsequence
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RemoveElementsFromArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Input
+            int n = int.Parse(Console.ReadLine());
+            int[] array = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                array[i] = int.Parse(Console.ReadLine());
+            }
+
+            int maxSubseqLength = 0;
+            int[] helperArray = new int[n]; // helper array storing sequence length for each element
+
+            // for each EndPoint access each subsequence in the array  
+            for (int endPoint = 0; endPoint < array.Length; endPoint++)
+            {
+                maxSubseqLength = 0;
+
+                // for each start point then set a StartPoint => 0 to EndPoint                              
+                for (int startPoint = 0; startPoint < endPoint; startPoint++)
+                {
+                    // if current StartPoint element is smaller than the at EndPoint they are members of the same sequence
+                    if (array[startPoint] <= array[endPoint]
+                        && helperArray[startPoint] >= maxSubseqLength)
+                    // if the length for the element before is larger than the current MaxSequenceLength
+                    {
+                        // max equals the length for the current element
+                        maxSubseqLength = helperArray[startPoint];
+                    }
+                }
+
+                // Longest existing sequence
+                helperArray[endPoint] = maxSubseqLength + 1;
+            }
+
+            // Find the max length of subsequence amongst all subsequence lengths
+            maxSubseqLength = 0;
+            for (int i = 0; i < n; i++)
+            {
+                // Length stored in the helper array                      
+                if (helperArray[i] > maxSubseqLength)
+                {
+                    maxSubseqLength = helperArray[i];
+                }
+            }
+
+            // Output
+            Console.WriteLine(n - maxSubseqLength);
+        }
+    }
+}
---
+++ C# Fundamentals - 2/1.Arrays/SieveOfEratostenes/SieveOfEratostenes/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SieveOfEratostenes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            bool[] primes = new bool[n + 1]; // False by default
+
+            // Find all prime numbers in the range [1...n]
+            for (int i = 2; i <= Math.Sqrt(n); i++) //n + 1 = primes.Length
+            {
+                // Skip those numbers that are not prime
+                if (primes[i] == false)
+                {
+                    for (int j = i * i; j <= n; j += i)
+                    {
+                        primes[j] = true;
+                    }
+                }
+            }
+
+            for (int i = n; i >= 2; i--)
+            {
+                if (!primes[i])
+                {
+                    Console.WriteLine(i);
+                    break;
+                }
+            }
+        }
+    }
+}
