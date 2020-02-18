b4d9560335fd34ec43d2ab9e090028cea5c1fb55
Exodary <krasimir_zlatkov@mail.bg>
2020-02-18 17:34:31 +0200

    Adding Solved Tasks For Methods HW

---
+++ C# Fundamentals - 2/3.Methods/GetLargestNumber/GetLargestNumber/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GetLargestNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int maximum = GetMax(GetMax(numbers[0], numbers[1]), numbers[2]);
+
+            Console.WriteLine(maximum);
+        }
+
+        static int GetMax(int firstNumber, int secondNumber)
+        {
+            if(firstNumber > secondNumber)
+            {
+                return firstNumber;
+            }
+            else
+            {
+                return secondNumber;
+            }
+        }
+    }
+}
