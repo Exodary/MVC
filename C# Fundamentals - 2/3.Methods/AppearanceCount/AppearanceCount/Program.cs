0e3d50b969f45432c51580ac81a8e4bf4b24a140
Exodary <krasimir_zlatkov@mail.bg>
2020-02-19 18:34:12 +0200

    More Solved Tasks For Methods HW

---
+++ C# Fundamentals - 2/3.Methods/AppearanceCount/AppearanceCount/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppearanceCount
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] inputArray = ArrayReader();
+
+            int numberToCount = int.Parse(Console.ReadLine());
+
+            int countResult = Counter(numberToCount, inputArray);
+
+            Console.WriteLine(countResult);
+
+        }
+
+        static int[] ArrayReader()
+        {
+            int sizeOfArray = int.Parse(Console.ReadLine());
+
+            string[] inputText = Console.ReadLine()
+                .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+            int[] numbers = new int[sizeOfArray];
+
+            for(int i = 0; i < numbers.Length - 1; i++)
+            {
+                numbers[i] = int.Parse(inputText[i]);
+            }
+
+            return numbers;
+        }
+
+        static int Counter(int number,params int[] array)
+        {
+            int counter = 0;
+
+            for(int i = 0; i < array.Length - 1; i++)
+            {
+                if(array[i] == number)
+                {
+                    counter++;
+                }
+            }
+
+            return counter;
+        }
+    }
+}
---
+++ C# Fundamentals - 2/3.Methods/DrawShape/DrawShape/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DrawShape
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            drawShape(number);
+        }
+
+        static void drawShape(int number)
+        {
+            for (int i = 1; i <= number; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write("{0} ", j);
+                }
+
+                Console.WriteLine();
+            }
+
+            for (int i = 1; i < number; i++)
+            {
+                for (int j = 1; j <= number - i; j++)
+                {
+                    Console.Write("{0} ", j);
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
