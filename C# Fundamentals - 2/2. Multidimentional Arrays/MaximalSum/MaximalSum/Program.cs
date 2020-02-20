81a93006450bbb1d4e39f8ffd7638899896e79b4
Exodary <krasimir_zlatkov@mail.bg>
2020-02-20 19:05:08 +0200

    Addding Solved Tasks For Multidimantional Arrays HW

---
+++ C# Fundamentals - 2/2. Multidimentional Arrays/MaximalSum/MaximalSum/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Read the matrix dimensions
+        string[] sizes = Console.ReadLine().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+        int rows = int.Parse(sizes[0]);
+        int cols = int.Parse(sizes[1]);
+
+        // Create (Allocate) the matrix
+        int[,] matrix = new int[rows, cols];
+
+        // Enter the matrix elements
+        for (int row = 0; row < rows; row++)
+        {
+            string[] inputRows = Console.ReadLine().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+            for (int col = 0; col < cols; col++)
+            {
+                matrix[row, col] = int.Parse(inputRows[col]);
+            }
+        }
+
+        // Find the maximal sum platform of size 3 x 3
+        int bestSum = int.MinValue;
+        int bestRow = 0;
+        int bestCol = 0;
+
+        for (int row = 0; row < matrix.GetLength(0) - 2; row++)
+        {
+            for (int col = 0; col < matrix.GetLength(1) - 2; col++)
+            {
+                int sum = matrix[row, col] + matrix[row, col + 1] + matrix[row, col + 2] +
+                          matrix[row + 1, col] + matrix[row + 1, col + 1] + matrix[row + 1, col + 2] +
+                          matrix[row + 2, col] + matrix[row + 2, col + 1] + matrix[row + 2, col + 2];
+
+                if (sum > bestSum)
+                {
+                    bestSum = sum;
+                    bestRow = row;
+                    bestCol = col;
+                }
+            }
+        }
+
+        // Output
+        Console.WriteLine(bestSum);
+    }
+
+}
