db7fa28f57ee1644f3147d1a07b586708a74ed71
Exodary <krasimir_zlatkov@mail.bg>
2020-02-22 23:12:41 +0200

    Solved Tasks For Numeric System HW

---
+++ C# Fundamentals - 2/4. Numeric systems/BinaryShort/BinaryShort/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class ShortToBinaryConvertion
+{
+    static void Main()
+    {
+        short shortNumber = short.Parse(Console.ReadLine());
+
+        Console.WriteLine(ShortToBinary(shortNumber));
+    }
+
+    private static string ShortToBinary(short number)
+    {
+        string binaryNumber = string.Empty;
+
+        binaryNumber = Convert.ToString(number, 2).PadLeft(16, '0');
+
+        return binaryNumber;
+    }
+}
