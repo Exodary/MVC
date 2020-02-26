51db7123721726c72349af8b8dbf99082279e035
Exodary <krasimir_zlatkov@mail.bg>
2020-02-26 21:34:57 +0200

    Solved Tasks For Classes and Objects

---
+++ C# Fundamentals - 2/5. Using Classes and Objects/RandomNumbers/RandomNumbers/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class RandomNumbers
+{
+    static void Main()
+    {
+        Random rndGenerator = new Random();
+
+        for (int i = 0; i < 10; i++)
+        {
+            Console.WriteLine(rndGenerator.Next(100, 201));
+        }
+    }
+}
