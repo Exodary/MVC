3d196a685d90a61868a262067619c208babb7e1e
Exodary <krasimir_zlatkov@mail.bg>
2020-02-27 20:44:01 +0200

    More Tasks

---
+++ C# Fundamentals - 2/5. Using Classes and Objects/DayOfTheWeek/DayOfTheWeek/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class DayOfWeek
+{
+    static void Main()
+    {
+        // first way
+        Console.WriteLine(DateTime.Now.DayOfWeek);
+
+        // second way
+        Console.WriteLine(DateTime.Today.DayOfWeek);
+    }
+}
