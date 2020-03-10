4fbae3ded5bc7babee3aed4870ba565f5092faa1
Exodary <krasimir_zlatkov@mail.bg>
2020-03-10 22:39:51 +0200

    Adding Task For Method HW

---
+++ C# Fundamentals - 2/3.Methods/SquareOfNumber/SquareOfNumber/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace SquareOfNumber
+{
+    class Program
+    {
+        static void SquareOfNumber(int number)
+        {
+            int square = number * number;
+
+            Console.WriteLine(square);
+        }
+
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+
+            SquareOfNumber(num);
+        }
+    }
+}
