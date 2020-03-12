8e6773603a523e05beef11716b2300359f4472a7
Exodary <krasimir_zlatkov@mail.bg>
2020-03-12 11:58:19 +0200

    Solved Tasks for Methods HW

---
+++ C# Fundamentals - 2/3.Methods/ApperancCount/ApperancCount/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace ApperancCount
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int arraySize = int.Parse(Console.ReadLine());
+
+            string[] numberAsText = Console.ReadLine()
+                .Split(new char[] { ' ', ',', '.' }, StringSplitOptions.RemoveEmptyEntries);
+
+            int[] numbers = new int[arraySize];
+
+            for(int i = 0; i< numbers.Length;i++)
+            {
+                numbers[i] = int.Parse(numberAsText[i]);
+            }
+
+            int desiredNumber = int.Parse(Console.ReadLine());
+
+            int appCount = ApperanceCount(numbers, desiredNumber);
+
+            Console.WriteLine(appCount);
+        }
+
+        static int ApperanceCount(int[] array, int number)
+        {
+            int count = 0;
+
+            for(int i = 0; i < array.Length; i++)
+            {
+                if(array[i] == number)
+                {
+                    count++;
+                }
+            }
+
+            return count;
+        }
+    }
+}
---
+++ C# Fundamentals - 2/3.Methods/FirstLargerThanNeighbour/FirstLargerThanNeighbour/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+
+namespace FirstLargerThanNeighbour
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int arraySize = int.Parse(Console.ReadLine());
+
+            int[] numbers = new int[arraySize];
+
+            ArrayReader(arraySize, numbers);
+
+            int result = FirstLargerThenNeighbours(numbers);
+
+            Console.WriteLine(result);
+        }
+
+        static void ArrayReader(int length, params int[] array)
+        {
+            string[] textArray = Console.ReadLine()
+                .Split(new char[] { ' ', '.', ',' }, StringSplitOptions.RemoveEmptyEntries);
+
+            for (int i = 0; i < length; i++)
+            {
+                array[i] = int.Parse(textArray[i]);
+            }
+        }
+
+        static int FirstLargerThenNeighbours(params int[] array)
+        {
+            int index = 0;
+
+            for (int i = 1; i < array.Length - 1; i++)
+            {
+                if (array[i - 1] < array[i] && array[i + 1] < array[i])
+                {
+                    index = i;
+
+                    break;
+                }
+            }
+
+            return index;
+        }
+    }
+}
