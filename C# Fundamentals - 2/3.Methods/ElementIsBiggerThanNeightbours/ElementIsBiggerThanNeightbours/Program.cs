14a21049e85f8ab3eb10be7d613e1ad1f4cdf6e1
Exodary <krasimir_zlatkov@mail.bg>
2020-03-14 09:24:15 +0200

    Two more solved Tasks for Method HW

---
+++ C# Fundamentals - 2/3.Methods/ElementIsBiggerThanNeightbours/ElementIsBiggerThanNeightbours/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+
+namespace ElementIsBiggerThanNeightbours
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int length = int.Parse(Console.ReadLine());
+
+            int[] numbers = new int[length];
+
+            ArrayInput(length, numbers);
+
+            int counter = LargerThanNeighbour(numbers);
+
+            Console.WriteLine(counter);
+
+
+        }
+
+        static void ArrayInput(int length, params int[] array)
+        {
+            string[] textArray = Console.ReadLine()
+                .Split(new char[] { ' ', ',', '.' }, StringSplitOptions.RemoveEmptyEntries);
+
+            for(int i = 0; i < length; i++)
+            {
+                array[i] = int.Parse(textArray[i]);
+            }
+        }
+
+        static int LargerThanNeighbour(params int[] array)
+        {
+            int counter = 0;
+
+            for(int i = 1; i< array.Length - 1; i++)
+            {
+                if(array[i -1] < array[i] && array[i + 1] < array[i])
+                {
+                    counter++;
+                }
+            }
+
+            return counter;
+        }
+    }
+}
---
+++ C# Fundamentals - 2/3.Methods/ReversedNumber/ReversedNumber/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ReversedNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal number = decimal.Parse(Console.ReadLine());
+
+            Console.WriteLine(ReversedNumber(number));
+        }
+
+        static decimal ReversedNumber(decimal number)
+        {
+            string input = number.ToString();
+
+            string reversedInput = null;
+
+            for (int i = input.Length - 1; i >= 0; i--)
+            {
+                reversedInput += input[i];
+            }
+
+            return decimal.Parse(reversedInput);
+        }
+    }
+}
