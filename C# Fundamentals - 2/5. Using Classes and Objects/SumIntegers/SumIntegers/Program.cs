c31156170437355a0a9825d39bc8104882896efe
Exodary <krasimir_zlatkov@mail.bg>
2020-03-15 22:39:11 +0200

    Add solved task

---
+++ C# Fundamentals - 2/5. Using Classes and Objects/SumIntegers/SumIntegers/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SumIntegers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                string[] splitNumbers = Console.ReadLine()
+                    .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+                int sum = 0;
+
+                for (int i = 0; i < splitNumbers.Length; i++)
+                {
+                    sum += int.Parse(splitNumbers[i]);
+                }
+
+                Console.WriteLine(sum);
+            }
+        }
+    }
+}
