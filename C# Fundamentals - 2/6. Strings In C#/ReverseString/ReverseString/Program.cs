01ca9c638cd3ee8da400eb6c217bc235e791f3de
Exodary <krasimir_zlatkov@mail.bg>
2020-03-20 21:01:13 +0200

    Added String Tasks

---
+++ C# Fundamentals - 2/6. Strings In C#/ReverseString/ReverseString/Program.cs
@@ -0,0 +1,34 @@
+﻿/*  2. Reverse string    
+    Write a program that reads a string, reverses it and prints the result on the console.
+    Constraints: 1 <= size of string <= 10000
+    Sample tests:
+                    Input 	        Output
+                    sample 	        elpmas
+                    somestring 	    gnirtsemos
+ */
+
+using System;
+using System.Text;
+
+class ReverseString
+{
+    public static string ReversingString(string str)
+    {
+        var sb = new StringBuilder(); 
+        for (int i = str.Length - 1; i >= 0; i--)
+        {
+            sb.Append(str[i]);
+        }
+        return sb.ToString();
+    }
+
+    static void Main()
+    {
+        string str = Console.ReadLine();
+
+        string reversed = ReversingString(str);
+
+        Console.WriteLine(reversed);
+
+    }
+}
