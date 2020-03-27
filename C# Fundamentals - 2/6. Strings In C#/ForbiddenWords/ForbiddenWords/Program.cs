ec1299abb653381bfc31027e3e95339e1d84080f
Exodary <krasimir_zlatkov@mail.bg>
2020-03-27 11:05:50 +0200

    Another Task For String Added

---
+++ C# Fundamentals - 2/6. Strings In C#/ForbiddenWords/ForbiddenWords/Program.cs
@@ -0,0 +1,34 @@
+﻿/*  9. Forbidden words
+    We are given a string containing a list of forbidden words and a text containing some of these words. 
+    Write a program that replaces the forbidden words with asterisks.
+    Sample tests:
+    Input: 	
+    Microsoft announced its next generation PHP compiler today. It is based on .NET Framework 4.0 and is implemented as a dynamic language in CLR.
+    PHP CLR Microsoft
+    Output:
+    ********* announced its next generation *** compiler today. It is based on .NET Framework 4.0 and is implemented as a dynamic language in ***.
+ */
+
+using System;
+using System.Text;
+
+class ForbiddenWords
+{
+    static void Main()
+    {
+        string text = Console.ReadLine();
+        string[] forbiddenWords = Console.ReadLine()
+            .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+        StringBuilder asterisksReplacedText = new StringBuilder();
+
+        asterisksReplacedText.Append(text);
+
+        foreach (string word in forbiddenWords)
+        {
+            asterisksReplacedText.Replace(word, new string('*', word.Length));
+        }
+
+        Console.WriteLine(asterisksReplacedText);
+    }
+}
