c80788b7fb36832173616e246b3c06b9a2c320d4
Exodary <krasimir_zlatkov@mail.bg>
2020-03-30 23:55:57 +0300

    Another Task Solved

---
+++ C# Fundamentals - 2/6. Strings In C#/DateDifference/DateDifference/Program.cs
@@ -0,0 +1,28 @@
+﻿/*  16. Date difference
+    Write a program that reads two dates in the format: day.month.year 
+    and calculates the number of days between them.
+    Example:
+    Enter the first date: 27.02.2006
+    Enter the second date: 3.03.2006
+    Distance: 4 days
+ */
+
+using System;
+using System.Globalization;
+
+class DateDifference
+{
+    static void Main()
+    {
+        Console.Write("Enter the first date: ");
+        string startDate = Console.ReadLine();
+        Console.Write("Enter the second datе: ");
+        string endDate = Console.ReadLine();
+
+        DateTime start = DateTime.ParseExact(startDate, "d.M.yyyy", CultureInfo.InvariantCulture);
+        DateTime end = DateTime.ParseExact(endDate, "d.M.yyyy", CultureInfo.InvariantCulture);
+
+        Console.WriteLine("Distance: {0} days", Math.Abs((end - start).TotalDays));
+
+    }
+}
