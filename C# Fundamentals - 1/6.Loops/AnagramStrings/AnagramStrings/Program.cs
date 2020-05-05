3ae8263855908c7739e40a90ba0bb9d7928f788f
Exodary <krasimir_zlatkov@mail.bg>
2020-05-05 22:00:45 +0300

    Adding some more completed tasks

---
+++ C# Fundamentals - 1/6.Loops/AnagramStrings/AnagramStrings/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnagramStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstWord = Console.ReadLine().Trim();
+            string secondWord = Console.ReadLine().Trim();
+
+            CheckAnagram(firstWord, secondWord);
+        }
+
+        static void CheckAnagram(string firstWord, string secondWord)
+        {
+            firstWord.ToLower();
+            secondWord.ToLower();
+
+            char[] charsFirstWord = firstWord.ToCharArray();
+            char[] charsSecondWord = secondWord.ToCharArray();
+
+            Array.Sort(charsFirstWord);
+            Array.Sort(charsSecondWord);
+
+            string newFirstWord = new string(charsFirstWord);
+            string newSecondWord = new string(charsSecondWord);
+
+            if(newFirstWord == newSecondWord)
+            {
+                Console.WriteLine("Yes, the words {0} and {1} are anagrams",newFirstWord, newSecondWord);
+            }
+
+            else
+            {
+                Console.WriteLine("No, the words {0} and {1} aren't anagrams", newFirstWord, newSecondWord);
+            }
+        }
+    }
+}
---
+++ C# Fundamentals - 1/6.Loops/RemoveDuplicateCharacters/RemoveDuplicateCharacters/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string UserInput = Console.ReadLine();
+
+        string result = RemoveDuplicateChars(UserInput);
+
+        Console.WriteLine(result);
+        
+    }
+
+    static string RemoveDuplicateChars(string key)
+    {
+        string table = "";
+
+        string result = "";
+
+        foreach (char value in key)
+        {
+            if (table.IndexOf(value) == -1)
+            {
+                // Append to the table and the result.
+                table += value;
+                result += value;
+            }
+        }
+        return result;
+    }
+}
